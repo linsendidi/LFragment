e3a87713e90ac0343c9a8e0cd9b78db14a8754d9
linsendidi <linymdidi@gmail.com>
2017-03-17 23:14:06 +0800

    Lab02
    labwork

---
+++ app/src/main/java/cn/edu/cqu/myfargment/MainActivity.java
@@ -0,0 +1,17 @@
+package cn.edu.cqu.myfargment;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
